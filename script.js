cdb6e2475ad2b629b9d5bf57ccd9f709c8d6106d
Shlandie <justikas12@gmail.com>
2022-12-20 12:07:49 +0200

    pusher

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+function buttonpress(idin) {
+    let element = document.getElementById(idin);
+    let button = element.getElementsByTagName("button")[0];
+    let image = element.getElementsByTagName("img")[0];
+    let h4 = element.getElementsByTagName("h4")[0];
+    let p = element.getElementsByTagName("p")[0];
+
+
+
+    if (p.style.display == "block") {
+        p.style.display = "none";
+        h4.style.display = "block";
+        image.style.display = "flex";
+        element.style.backgroundColor = "var(--Sacramento-State-Green)";
+        element.getElementsByTagName("div")[0].remove();
+        button.classList.remove("button--pressed");
+        return;
+    }
+    button.className += " button--pressed";
+    button.classList.add("button--pressed");
+    p.style.display = "block";
+    h4.style.display = "none";
+    image.style.display = "none";
+    console.log(image);
+    p.insertAdjacentHTML("afterend", `
+                    <div class="footer__socials" style="padding: 0;">
+                    <a><img src="../IMG/icon-twitter.svg" style="width:100%;height:100%;border:none; border-radius:0;" alt="twitter"></a>
+                    <a><img src="../IMG/icon-linkedin.svg" style="width:100%;height:100%;border:none; border-radius:0;" alt="pinterest"></a>
+                    </div> `);
+}
+
 const validate = new window.JustValidate("#form");
 validate
     .addField(
